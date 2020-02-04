53c8068eee6a24ab57708b81091fe7d31f750d5c
paulpschreiber <paulpschreiber1@gmail.com>
2020-02-04 17:32:09 -0500

    Switched talons to TalonFX and added TelemetryService

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,23 +1,37 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.strykeforce.thirdcoast.talon.TalonFXItem;
+import org.strykeforce.thirdcoast.telemetry.TelemetryService;
 
 public class IntakeSubsystem extends SubsystemBase {
   public static int TALON_ID = 20;
 
-  public static TalonSRX intakeDrive;
+  public static TalonFX intakeDrive;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   //
   public IntakeSubsystem() {
-    intakeDrive = new TalonSRX(TALON_ID);
-    intakeDrive.configPeakCurrentLimit(35);
-    intakeDrive.configPeakCurrentDuration(40);
-    intakeDrive.configContinuousCurrentLimit(30);
+    intakeDrive = new TalonFX(TALON_ID);
+
+    TalonFXConfiguration talonConfig = new TalonFXConfiguration();
+    talonConfig.supplyCurrLimit.currentLimit = 40.0;
+    talonConfig.supplyCurrLimit.triggerThresholdCurrent = 45.0;
+    talonConfig.supplyCurrLimit.triggerThresholdTime = 40.0;
+    talonConfig.supplyCurrLimit.enable = true;
+
+    intakeDrive.configAllSettings(talonConfig);
+
+    TelemetryService telemetryService = RobotContainer.TELEMETRY;
+    telemetryService.stop();
+    telemetryService.register(new TalonFXItem(intakeDrive, "Intake"));
+    telemetryService.start();
   }
 
   public void stopIntake() {
