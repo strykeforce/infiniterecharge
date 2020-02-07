09fafe533896554aa466b27ce651ec6b5dccd9a4
paulpschreiber <paulpschreiber1@gmail.com>
2020-02-06 19:05:12 -0500

    Fixed "manifold" magazine

--- src/main/java/frc/robot/commands/magazine/StopMagazineCommand.java
+++ src/main/java/frc/robot/commands/magazine/StopMagazineCommand.java
@@ -1,13 +1,13 @@
-package frc.robot.commands.manifold;
+package frc.robot.commands.magazine;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.MagazineSubsystem;
 
-public class StopManifoldCommand extends InstantCommand {
+public class StopMagazineCommand extends InstantCommand {
   private MagazineSubsystem magazineSubsystem = RobotContainer.MAGAZINE;
 
-  public StopManifoldCommand() {
+  public StopMagazineCommand() {
     addRequirements(magazineSubsystem);
   }
 
