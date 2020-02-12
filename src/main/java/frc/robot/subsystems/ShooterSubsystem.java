14c8e7153bb60ac7f281970b5d18a85df5c52a9b
rabsamu <rabsamu@gmail.com>
2020-02-11 19:23:20 -0500

    Apply spotless

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -109,7 +109,7 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     hood.configForwardLimitSwitchSource(
-            LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.Disabled);
+        LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.Disabled);
 
     return didZero;
   }
@@ -127,7 +127,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
   public void setTurretAngle(double targetAngle) {
     if (targetAngle <= kWrapRange && turret.getSelectedSensorPosition() > kTurretMidpoint
-            || targetAngle < 0) {
+        || targetAngle < 0) {
       targetAngle += 360;
     }
     double setPoint = targetAngle * TURRET_TICKS_PER_DEGREE;
