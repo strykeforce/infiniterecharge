100679e6da8acf18d07db6631179d99db3a5a2b7
mwitcpalek <35316453+mwitcpalek@users.noreply.github.com>
2020-02-22 10:00:42 -0500

    Tele bug fixes

--- src/main/java/frc/robot/commands/sequences/ArmSequenceCommand.java
+++ src/main/java/frc/robot/commands/sequences/ArmSequenceCommand.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.MagazineConstants;
 import frc.robot.Constants.ShooterConstants;
-import frc.robot.commands.WaitCommand;
 import frc.robot.commands.magazine.RunMagazineCommand;
+import frc.robot.commands.magazine.WaitForMagazineBeamCommand;
 import frc.robot.commands.shooter.ArmShooterCommand;
 import frc.robot.commands.shooter.RunShooterCommand;
 import frc.robot.commands.shooter.SeekTargetCommand;
@@ -15,7 +15,7 @@ public class ArmSequenceCommand extends SequentialCommandGroup {
     addCommands(
         new RunMagazineCommand(MagazineConstants.kOpenloopArmReverse),
         new RunShooterCommand(ShooterConstants.kOpenloopArmReverse),
-        new WaitCommand(MagazineConstants.kArmTimeToShooterOn),
+        new WaitForMagazineBeamCommand(),
         new ParallelCommandGroup(new ArmShooterCommand(), new SeekTargetCommand()));
   }
 }
