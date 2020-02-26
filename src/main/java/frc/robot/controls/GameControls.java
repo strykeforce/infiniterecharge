f5b1bfb63215b7f32ee65e03e200f0464cb2f378
maxwellscott922 <41545083+maxwellscott922@users.noreply.github.com>
2020-02-25 19:47:56 -0500

    Changed intake triggers to the requested on Github
    The issue on github was solved by changing the triggers for the DPAD, work in progress.

--- src/main/java/frc/robot/controls/GameControls.java
+++ src/main/java/frc/robot/controls/GameControls.java
@@ -4,7 +4,9 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants;
 import frc.robot.commands.hood.HoodPositionCommand;
+import frc.robot.commands.intake.IntakeRunCommand;
 import frc.robot.commands.sequences.*;
 import frc.robot.commands.turret.SeekTargetCommand;
 import frc.robot.commands.turret.TurretOpenLoopCommand;
@@ -50,14 +52,59 @@ public class GameControls {
           }
         };
 
+    Trigger UpDPAD =
+        new Trigger() {
+          @Override
+          public boolean get() {
+
+            int returnValue = 0;
+            if (controller.getPOV(0) == 315) {
+              returnValue = 315;
+            }
+            if (controller.getPOV(0) == 0) {
+              returnValue = 0;
+            }
+
+            if (controller.getPOV(0) == 45) {
+              returnValue = 45;
+            }
+
+            return controller.getPOV(0) == returnValue;
+          }
+        };
+
+    Trigger DownDPAD =
+        new Trigger() {
+          @Override
+          public boolean get() {
+
+            int returnValue = 0;
+            if (controller.getPOV(0) == 135) {
+              returnValue = 135;
+            }
+            if (controller.getPOV(0) == 180) {
+              returnValue = 180;
+            }
+
+            if (controller.getPOV(0) == 225) {
+              returnValue = 225;
+            }
+
+            return controller.getPOV(0) == returnValue;
+          }
+        };
+
     new JoystickButton(controller, Button.kB.value).whenPressed(new AutoIntakeCmdGroup());
     new JoystickButton(controller, Button.kB.value)
         .whenReleased(new StopIntakeAndMagazineCommandGroup());
     new JoystickButton(controller, Button.kY.value).whenPressed(new ArmSequenceCommand());
     new JoystickButton(controller, Button.kStart.value).whenPressed(new SeekTargetCommand());
     new JoystickButton(controller, Button.kBack.value).whenPressed(new HoodPositionCommand(5000));
-    dPad.whenActive(new ReverseIntakeAndMagazineCommandGroup());
+    UpDPAD.whenActive(new ReverseIntakeAndMagazineCommandGroup());
+    DownDPAD.whenActive(new IntakeRunCommand(Constants.IntakeConstants.kEjectSpeed));
+
     dPad.whenInactive(new StopIntakeAndMagazineCommandGroup());
+
     new JoystickButton(controller, Button.kX.value).whenPressed(new StopShootCommand());
 
     LeftStickLeft.whenActive(new TurretOpenLoopCommand(0.3));
