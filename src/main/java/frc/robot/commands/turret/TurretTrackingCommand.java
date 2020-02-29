38b34d52eb20b0cd486c44e7865c898c902caca3
rabsamu <rabsamu@gmail.com>
2020-02-28 21:51:02 -0500

    Add two step seeking

--- src/main/java/frc/robot/commands/turret/TurretTrackingCommand.java
+++ src/main/java/frc/robot/commands/turret/TurretTrackingCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.turret;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.controls.DriverControls;
 import frc.robot.subsystems.TurretSubsystem;
@@ -13,7 +14,10 @@ public class TurretTrackingCommand extends CommandBase {
   private static final VisionSubsystem VISION = RobotContainer.VISION;
   private static DriverControls DRIVER_CONTROLS;
 
-  double strafeFactor = 5.45; // theoretical 7.45
+  private double strafeFactor = 5.45; // theoretical 7.45
+  private TrackingState state;
+  private int attemptNum;
+  private final int ATTEMPT_LIMIT = 2;
 
   public Logger logger = LoggerFactory.getLogger("Turret Track Command");
 
@@ -23,15 +27,63 @@ public class TurretTrackingCommand extends CommandBase {
 
   @Override
   public void initialize() {
+    state = TrackingState.SEEK_LEFT;
+    attemptNum = 1;
     VISION.setTrackingEnabled(true);
     DRIVER_CONTROLS = RobotContainer.CONTROLS.getDriverControls();
   }
 
   @Override
   public void execute() {
-    if (VISION.isTargetValid()) {
-      TURRET.rotateTurret(-0.95 * VISION.getOffsetAngle() + getStrafeAdjustment());
-    } else TURRET.seekTarget();
+    // stop looking after two sweeps
+    if (attemptNum > ATTEMPT_LIMIT) {
+      VISION.setTrackingEnabled(false);
+      return;
+    }
+
+    switch (state) {
+      case SEEK_LEFT:
+
+        // switch to tracking if target is detected
+        if (VISION.isTargetValid()) {
+          state = TrackingState.HAS_TARGET;
+          logger.info("Target acquired");
+          break;
+        }
+
+        // wait to reach the left sweep point, then switch
+        TURRET.seekTarget(Constants.TurretConstants.kSweepRange);
+        if (TURRET.turretInRange(2)) {
+          logger.info("Seeking right");
+          state = TrackingState.SEEK_RIGHT;
+        }
+        break;
+      case SEEK_RIGHT:
+        // switch to tracking if target is detected
+        if (VISION.isTargetValid()) {
+          state = TrackingState.HAS_TARGET;
+          logger.info("Target acquired");
+          break;
+        }
+
+        // wait to reach the right sweep point, then switch, mark new attempt
+        TURRET.seekTarget(-Constants.TurretConstants.kSweepRange);
+        if (TURRET.turretInRange(2)) {
+          attemptNum++;
+          logger.info("Seeking left");
+          state = TrackingState.SEEK_LEFT;
+        }
+        break;
+      case HAS_TARGET:
+        // continue tracking until target is lost
+        attemptNum = 1;
+        if (VISION.isTargetValid()) {
+          TURRET.rotateTurret(-0.95 * VISION.getOffsetAngle() + getStrafeAdjustment());
+        } else {
+          state = TrackingState.SEEK_LEFT;
+        }
+        break;
+    }
   }
 
   @Override
@@ -45,5 +97,11 @@ public class TurretTrackingCommand extends CommandBase {
       return strafe * strafeFactor;
     }
     return 0;
+  }
+
+  private enum TrackingState {
+    SEEK_RIGHT,
+    SEEK_LEFT,
+    HAS_TARGET
   }
 }
