b1640c3c039bb4658168c8e89997ba6a13576ee8
rabsamu <rabsamu@gmail.com>
2021-03-11 20:30:47 -0500

    Edit bounce path for new starting position

--- src/main/java/frc/robot/commands/auto/PathDriveCommand.java
+++ src/main/java/frc/robot/commands/auto/PathDriveCommand.java
@@ -28,7 +28,6 @@ public class PathDriveCommand extends CommandBase {
   @Override
   public void initialize() {
     isFirstExecute = true;
-    driveSubsystem.offsetGyro(-targetYaw);
     driveSubsystem.startPath(trajectory, targetYaw);
     logger.info("Starting pathing...");
   }
@@ -54,7 +53,6 @@ public class PathDriveCommand extends CommandBase {
   public void end(boolean interrupted) {
     logger.info("Stopping pathing; Interruption: " + interrupted);
     driveSubsystem.drive(0, 0, 0);
-    driveSubsystem.offsetGyro(targetYaw);
     driveSubsystem.setDriveMode(DriveMode.OPEN_LOOP);
   }
 }
